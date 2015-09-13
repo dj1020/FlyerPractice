b1879b4d2f9eb47c1a195dd8d94dd66e13408570
Ken Lin <ken.lin@cloud-interactive.com>
2015-09-13 09:44:58 +0800

    init layout

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Project Flyer</title>
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
+</head>
+<body>
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+</body>
+</html>
